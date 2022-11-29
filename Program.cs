0c8085a8d32b4ea91597f2f24a41b2e696817e5c
Felipe Freitas dos Santos Silva <82713076+felipesansi@users.noreply.github.com>
2022-11-29 20:15:40 -0300

    enviado novamente

--- Program.cs
+++ Program.cs
@@ -11,9 +11,20 @@ namespace Aula14
         static void Main(string[] args)
         {
             Manipula arquivo = new Manipula();
-            Aluno a = new Aluno(6317602, "Felipe Freitas", "1 ADS", "233.620.578-30");
-            arquivo.Gravar(a.RA + ";" + a.Nome + ";" + a.Turma + ";" + a.Cpf);
+            //Aluno a = new Aluno(6317603, "Vinicius", "1 ADS", "233.720.578-30");
+            //arquivo.Gravar(a.RA + ";" + a.Nome + ";" + a.Turma + ";" + a.Cpf);
+            List<Aluno> Alunos = arquivo.ler();
+            string texto = null;
+            foreach(Aluno fuçano in Alunos)
+            {
+                texto += " \nRa =" +fuçano.RA + "- Nome =" +fuçano.Nome + " - Curso = "+fuçano.Turma + " - CPF = " +fuçano.Cpf;
+            }
+             Aluno a = new Aluno(6317699, "Vinicius", "1 ADM", "233.720.578-30");
+            arquivo.Alterar(a);
+            Console.WriteLine(texto);
             Console.ReadKey();
+
         }
+        
     }
 }
