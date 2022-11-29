8078d860df5370062efee6fe50278fb6fe0ce340
Felipe Freitas dos Santos Silva <82713076+felipesansi@users.noreply.github.com>
2022-11-29 20:15:40 -0300

    enviado novamente

--- Manipula.cs
+++ Manipula.cs
@@ -10,15 +10,15 @@ namespace Aula14
 {
     public class Manipula
     {
-        private string caminho = "D:/aula14/Dados.txt";
+        private string caminho = "E://aula14/Dados.txt";
 
         public void Gravar(string texto)
         {
             StreamWriter sw = new StreamWriter(caminho);
             sw.WriteLine(texto);
             sw.Close();
-            
-        } 
+
+        }
         public List<Aluno> ler()
         {
             var list = new List<Aluno>();
@@ -26,14 +26,44 @@ namespace Aula14
             {
                 StreamReader sr = File.OpenText(caminho);
                 string linha;
-                while((linha=sr.ReadLine()) != null)
+                while ((linha = sr.ReadLine()) != null)
                 {
                     var arquivo = linha.Split(';');
-                    Aluno alguem = new Aluno(int.Parse(arquivo[0]), arquivo[1], arquivo[2],arquivo[3]);
-                   list.Add(alguem);
+                    Aluno alguem = new Aluno(int.Parse(arquivo[0]), arquivo[1], arquivo[2], arquivo[3]);
+                    list.Add(alguem);
                 }
             }
             return list;
+        }
+        public void Alterar(Aluno a)
+        {
+            string texto = null;
+            if (File.Exists(caminho))
+            {
+                using (StreamReader sr = new StreamReader(caminho))
+                {
+                    string linha;
+                    while ((linha = sr.ReadLine()) != null)
+                    {
+                        var arquivo = linha.Split(';');
+                        if (int.Parse(arquivo[0]) == a.RA)
+                        {
+                            string nova = a.RA + ";" + a.Nome + ";" + a.Turma + ";" + a.Cpf + "\n";
+                            texto += nova;
+                        }
+                        else
+                        {
+                            texto += linha + "\n";
+                        }
+                    }sr.Close();
+                    File.Delete(caminho);
+                    StreamWriter sw = new StreamWriter(caminho);
+                    sw.WriteLine(texto);
+                    sw.Close(); 
+                }
+                
+
+            }
         }
     }
 }
