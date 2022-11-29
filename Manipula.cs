bda85f7906708cced562b905d0c0cbb148ccf457
felipe freitas dos santos silva <felipesansi2012@hotmail.com>
2022-11-28 21:28:24 -0300

    Adicionar arquivos de projeto.

---
+++ Manipula.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula14
+{
+    public class Manipula
+    {
+        private string caminho = "D:/aula14/Dados.txt";
+
+        public void Gravar(string texto)
+        {
+            StreamWriter sw = new StreamWriter(caminho);
+            sw.WriteLine(texto);
+            sw.Close();
+            
+        } 
+        public List<Aluno> ler()
+        {
+            var list = new List<Aluno>();
+            if (File.Exists(caminho))
+            {
+                StreamReader sr = File.OpenText(caminho);
+                string linha;
+                while((linha=sr.ReadLine()) != null)
+                {
+                    var arquivo = linha.Split(';');
+                    Aluno alguem = new Aluno(int.Parse(arquivo[0]), arquivo[1], arquivo[2],arquivo[3]);
+                   list.Add(alguem);
+                }
+            }
+            return list;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Manipula arquivo = new Manipula();
+            Aluno a = new Aluno(6317602, "Felipe Freitas", "1 ADS", "233.620.578-30");
+            arquivo.Gravar(a.RA + ";" + a.Nome + ";" + a.Turma + ";" + a.Cpf);
+            Console.ReadKey();
+        }
+    }
+}
